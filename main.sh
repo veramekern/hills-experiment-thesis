772f188ac771435a3c349a2f0b7529ff09d205b7
veramekern <veertje.m@gmail.com>
2016-11-18 15:20:20 +0100

    Included debug option

--- main.sh
+++ main.sh
@@ -4,20 +4,22 @@ echo -n "Enter subject ID: "
 read subject_ID
 echo -n "Choose condition: diffuse (d), clustered (c), or random (r): "
 read condition
+echo -n "Debug (f = false, t = true): "
+read debug
 
-./EBR_1.py $subject_ID $condition
+./EBR_1.py $subject_ID $condition $debug
 
-./scrabble_practice.py  $subject_ID $condition
-./scrabble_pretest.py $subject_ID $condition
+./scrabble_practice.py  $subject_ID $condition $debug
+./scrabble_pretest.py $subject_ID $condition $debug
 
-./visual_foraging_practice.py $subject_ID $condition
-./intro_foraging.py $subject_ID $condition
-./visual_foraging.py $subject_ID $condition
+./visual_foraging_practice.py $subject_ID $condition $debug
+./intro_foraging.py $subject_ID $condition $debug
+./visual_foraging.py $subject_ID $condition $debug
 
-./EBR_2.py $subject_ID $condition
+./EBR_2.py $subject_ID $condition $debug
 
-./scrabble_posttest.py $subject_ID $condition
+./scrabble_posttest.py $subject_ID $condition $debug
 
-./EBR_3.py $subject_ID $condition
+./EBR_3.py $subject_ID $condition $debug
 
 ./bisbas.py $subject_ID
